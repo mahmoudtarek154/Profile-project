ab972b5e59118d11f75d7172e4219e7f8f1f1d0f
mahmoudtarek154 <mahmoudtarek15444@gmail.com>
2025-01-17 20:17:41 +0200

    start-framework

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path:'home' , component: HomeComponent , title:'home page'},
+    {path:'about' , component: AboutComponent , title:'about page'},
+    {path:'portfolio' , component: PortfolioComponent , title:'portfolio page'},
+    {path:'contact' , component: ContactComponent, title:'contact page'},
+    {path:'**' , component: NotfoundComponent , title:'Not Found '},
+];
